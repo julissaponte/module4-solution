f2d9b8fa3ce3d20c380cc37c19ad4756b24ce32f
Julissa Ponte <u201713771@upc.edu.pe>
2022-06-23 03:17:32 -0500

    first commit

---
+++ SpeakHello.js
@@ -0,0 +1,13 @@
+(function (windows) {
+  
+  var helloSpeaker = new Object();
+
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
